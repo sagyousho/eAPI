a776412fbaf1dedc0634104c2861e94715b28648
sagyousho <sagyousho.nyumon@gmail.com>
2022-04-17 16:14:31 +0900

    seeding part for Product and Review

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Model\Product;
+use App\Model\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $this->call(ProductSeeder::class);
+        $this->call(ReviewSeeder::class);
     }
 }
